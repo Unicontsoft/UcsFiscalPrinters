4439a0ad5b843138e57b85c0695a2fafe928d389
Vlad Vissoultchev <wqweto@gmail.com>
2019-10-28 10:41:43 +0200

    Fix build scripts to pass /outdir param to VB6 compiler

--- contrib/UcsFPHub/scripts/build.bat
+++ contrib/UcsFPHub/scripts/build.bat
@@ -1,40 +1,40 @@
 @echo off
 setlocal
-for %%i in ("%~dp0.") do set file_dir=%%~dpnxi
-for %%i in ("%file_dir%\..\src\.") do set src_dir=%%~dpnxi
-for %%i in ("%file_dir%\..\..\..\bin\.") do set bin_dir=%%~dpnxi
-set output_exe=%bin_dir%\UcsFPHub.exe
-set VbCodeLines="%bin_dir%\VbCodeLines.exe"
-set Ummm="%bin_dir%\UMMM.exe"
-set mt=C:\Work\BuildTools\UMMM\mt.exe
-if not exist "%mt%" set mt=mt.exe
-set Vb6="%ProgramFiles%\Microsoft Visual Studio\VB98\VB6.EXE"
-if not exist %Vb6% set Vb6="%ProgramFiles(x86)%\Microsoft Visual Studio\VB98\VB6.EXE"
-set log_file=%file_dir%\compile.out
+for %%i in ("%~dp0.") do set "file_dir=%%~dpnxi"
+for %%i in ("%file_dir%\..\src\.") do set "src_dir=%%~dpnxi"
+for %%i in ("%file_dir%\..\..\..\bin\.") do set "bin_dir=%%~dpnxi"
+set "output_exe=%bin_dir%\UcsFPHub.exe"
+set "VbCodeLines=%bin_dir%\VbCodeLines.exe"
+set "Ummm=%bin_dir%\UMMM.exe"
+set "mt=C:\Work\BuildTools\UMMM\mt.exe"
+if not exist "%mt%" set "mt=mt.exe"
+set "Vb6=%ProgramFiles%\Microsoft Visual Studio\VB98\VB6.EXE"
+if not exist "%Vb6%" set "Vb6=%ProgramFiles(x86)%\Microsoft Visual Studio\VB98\VB6.EXE"
+set "log_file=%file_dir%\compile.out"
 
 echo Cleanup %file_dir%...
-for %%i in (%file_dir%\*.*) do (if not "%%~nxi"=="build.bat" if not "%%~nxi"=="UcsFPHub.ini" del %%i > nul)
-rd /s /q %file_dir%\Shared 2>&1
-mkdir %file_dir%\Shared
+for %%i in ("%file_dir%\*.*") do (if not "%%~nxi"=="build.bat" if not "%%~nxi"=="UcsFPHub.ini" del "%%i" > nul)
+rd /s /q "%file_dir%\Shared" 2>&1
+mkdir "%file_dir%\Shared"
 
 echo Copy sources from %src_dir%...
-for %%i in (%src_dir%\*.bas;%src_dir%\*.cls;%src_dir%\*.frm;%src_dir%\*.frx;%src_dir%\*.vbp) do (copy %%i %file_dir% > nul)
-for %%i in (%src_dir%\Shared\*.bas;%src_dir%\Shared\*.cls) do (copy %%i %file_dir%\Shared > nul)
+for %%i in ("%src_dir%\*.bas";"%src_dir%\*.cls";"%src_dir%\*.frm";"%src_dir%\*.frx";"%src_dir%\*.vbp") do (copy "%%i" "%file_dir%" > nul)
+for %%i in ("%src_dir%\Shared\*.bas";"%src_dir%\Shared\*.cls") do (copy "%%i" "%file_dir%\Shared" > nul)
 
 echo Put lines to sources in %file_dir%...
-for %%i in (%file_dir%\*.vbp) do (%VbCodeLines% %%i)
+for %%i in ("%file_dir%\*.vbp") do (start "" /w "%VbCodeLines%" %%i)
 
 echo Compiling to %bin_dir%...
-for %%i in (%file_dir%\*.vbp) do (
-    del %log_file% > nul 2>&1
-    %Vb6% /make %%i /out %log_file%
-    findstr /r /C:"Build of '.*' succeeded" %log_file% || (type %log_file% 1>&2 & exit /b 1)
+for %%i in ("%file_dir%\*.vbp") do (
+    del "%log_file%" > nul 2>&1
+    "%Vb6%" /make "%%i" /out "%log_file%" /outdir "%bin_dir%"
+    findstr /r /c:"Build of '.*' succeeded" "%log_file%" || (type "%log_file%" 1>&2 & exit /b 1)
 )
 
 echo Embedding manifest in %output_exe%...
-%Ummm% %file_dir%\UcsFPHub.ini
-%mt% -nologo -manifest "%file_dir%\UcsFPHub.ini.manifest" -outputresource:"%output_exe%;1"
+"%Ummm%" "%file_dir%\UcsFPHub.ini"
+"%mt%" -nologo -manifest "%file_dir%\UcsFPHub.ini.manifest" -outputresource:"%output_exe%;1"
 
-git checkout -- %bin_dir%\..\src\Shared\mdJson.bas
+git checkout -- "%bin_dir%\..\src\Shared\mdJson.bas"
 
 echo Done.
--- scripts/build.bat
+++ scripts/build.bat
@@ -1,36 +1,36 @@
 @echo off
 setlocal
-for %%i in ("%~dp0.") do set file_dir=%%~dpnxi
-for %%i in ("%file_dir%\..\src\.") do set src_dir=%%~dpnxi
-for %%i in ("%file_dir%\..\bin\.") do set bin_dir=%%~dpnxi
-set VbCodeLines=start /w "%bin_dir%\VbCodeLines.exe"
-set Vb6="%ProgramFiles%\Microsoft Visual Studio\VB98\VB6.EXE"
-if not exist %Vb6% set Vb6="%ProgramFiles(x86)%\Microsoft Visual Studio\VB98\VB6.EXE"
-set log_file=%file_dir%\compile.out
+for %%i in ("%~dp0.") do set "file_dir=%%~dpnxi"
+for %%i in ("%file_dir%\..\src\.") do set "src_dir=%%~dpnxi"
+for %%i in ("%file_dir%\..\bin\.") do set "bin_dir=%%~dpnxi"
+set "VbCodeLines=%bin_dir%\VbCodeLines.exe"
+set "Vb6=%ProgramFiles%\Microsoft Visual Studio\VB98\VB6.EXE"
+if not exist "%Vb6%" set "Vb6=%ProgramFiles(x86)%\Microsoft Visual Studio\VB98\VB6.EXE"
+set "log_file=%file_dir%\compile.out"
 
 echo Cleanup %file_dir%...
-for %%i in (%file_dir%\*.*) do (if not "%%~nxi"=="build.bat" del %%i > nul)
-rd /s /q %file_dir%\Connectors 2>&1
-mkdir %file_dir%\Connectors
-rd /s /q %file_dir%\Protocols 2>&1
-mkdir %file_dir%\Protocols
-rd /s /q %file_dir%\Shared 2>&1
-mkdir %file_dir%\Shared
+for %%i in ("%file_dir%\*.*") do (if not "%%~nxi"=="build.bat" del "%%i" > nul)
+rd /s /q "%file_dir%\Connectors" 2>&1
+mkdir "%file_dir%\Connectors"
+rd /s /q "%file_dir%\Protocols" 2>&1
+mkdir "%file_dir%\Protocols"
+rd /s /q "%file_dir%\Shared" 2>&1
+mkdir "%file_dir%\Shared"
 echo Copy sources from %src_dir%...
-for %%i in (%src_dir%\*.bas;%src_dir%\*.cls;%src_dir%\*.frm;%src_dir%\*.frx;%src_dir%\*.vbp) do (copy %%i %file_dir% > nul)
+for %%i in ("%src_dir%\*.bas";"%src_dir%\*.cls";"%src_dir%\*.frm";"%src_dir%\*.frx";"%src_dir%\*.vbp") do (copy "%%i" "%file_dir%" > nul)
 echo Copy sources from %src_dir%\Connectors...
-for %%i in (%src_dir%\Connectors\*.bas;%src_dir%\Connectors\*.cls) do (copy %%i %file_dir%\Connectors > nul)
+for %%i in ("%src_dir%\Connectors\*.bas";"%src_dir%\Connectors\*.cls") do (copy "%%i" "%file_dir%\Connectors" > nul)
 echo Copy sources from %src_dir%\Protocols...
-for %%i in (%src_dir%\Protocols\*.bas;%src_dir%\Protocols\*.cls) do (copy %%i %file_dir%\Protocols > nul)
+for %%i in ("%src_dir%\Protocols\*.bas";"%src_dir%\Protocols\*.cls") do (copy "%%i" "%file_dir%\Protocols" > nul)
 echo Copy sources from %src_dir%\Shared...
-for %%i in (%src_dir%\Shared\*.bas;%src_dir%\Shared\*.cls) do (copy %%i %file_dir%\Shared > nul)
+for %%i in ("%src_dir%\Shared\*.bas";"%src_dir%\Shared\*.cls") do (copy "%%i" "%file_dir%\Shared" > nul)
 echo Put lines to sources in %file_dir%...
-for %%i in (%file_dir%\*.vbp) do (%VbCodeLines% %%i)
+for %%i in ("%file_dir%\*.vbp") do (start "" /w "%VbCodeLines%" %%i)
 echo Compiling to %bin_dir%...
 attrib -r "%bin_dir%\*.*"
-for %%i in (%file_dir%\*.vbp) do (
-    del %log_file% > nul 2>&1
-    %Vb6% /make %%i /out %log_file%
-    findstr /r /C:"Build of '.*' succeeded" %log_file% || (type %log_file% 1>&2 & exit /b 1)
+for %%i in ("%file_dir%\*.vbp") do (
+    del "%log_file%" > nul 2>&1
+    "%Vb6%" /make "%%i" /out "%log_file%" /outdir "%bin_dir%"
+    findstr /r /c:"Build of '.*' succeeded" "%log_file%" || (type "%log_file%" 1>&2 & exit /b 1)
 )
 echo Done.
